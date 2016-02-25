f6166e2f613e41a443fdbc3a3cc69c7b8c4113c8
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2016-02-25 17:09:08 +0100

    Added a game that match the requirements of the project: Playable with multiple ghosts.
    Also added an exception to manage if there is less than 2
    players or more than 4 players.
    
    Finally, I added a method in GameFactory to create instances of MultiplayerWithoutPacmanGame.

---
+++ src/main/java/nl/tudelft/jpacman/game/MultiplayerWithoutPacmanGame.java
@@ -0,0 +1,41 @@
+package nl.tudelft.jpacman.game;
+
+import com.google.common.collect.ImmutableList;
+import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.level.PlayerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by julien on 25/02/16.
+ */
+public class MultiplayerWithoutPacmanGame extends Game {
+    /**
+     * The level of this game.
+     */
+    private final Level level;
+    private final ArrayList<Player> players;
+
+    /**
+     * Creates an instance of a game that is played by at least 2 players
+     * and at most 4 players.
+     * @param players - The players list for the game.
+     * @param level - The level to use for the game.
+     */
+    protected MultiplayerWithoutPacmanGame(ArrayList<Player> players, Level level){
+        this.level = level;
+        this.players = players;
+    }
+
+    @Override
+    public List<Player> getPlayers() {
+        return ImmutableList.copyOf(this.players);
+    }
+
+    @Override
+    public Level getLevel() {
+        return this.level;
+    }
+}
