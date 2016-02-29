30fd2f2c747c645a89de9f94e54547fa7d22d375
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2016-02-29 14:50:58 +0100

    Implemented tests for MultiPlayerCollisions.

--- src/main/java/nl/tudelft/jpacman/level/MultiPlayerCollisions.java
+++ src/main/java/nl/tudelft/jpacman/level/MultiPlayerCollisions.java
@@ -18,23 +18,31 @@ public class MultiPlayerCollisions extends PlayerCollisions {
     }
 
     private void hunterColliding(Hunter hunter, Unit collidedOn) {
-        if (collidedOn instanceof Hunter) {
+        if (collidedOn instanceof Hunter)
             hunterVersusHunter(hunter, (Hunter) collidedOn);
-        }
+        else if (collidedOn instanceof Pellet)
+            hunterVersusPellet(hunter, (Pellet) collidedOn);
     }
 
     private void hunterVersusHunter(Hunter hunter1, Hunter hunter2) {
         if (hunter1.isHunting() && hunter2.isAlive())
             hunter1.kill(hunter2);
-
-        if (hunter2.isHunting() && hunter1.isAlive())
+        else if (hunter2.isHunting() && hunter1.isAlive())
             hunter2.kill(hunter1);
+    }
+
+    private void hunterVersusPellet(Hunter hunter, Pellet pellet) {
+        if(!hunter.isHunting()) {
+            pellet.leaveSquare();
+            if (hunter instanceof HunterPlayer) {
+                ((HunterPlayer) hunter).addPoints(pellet.getValue());
+            }
+        }
     }
 
     @Override
     public void playerVersusPellet(Player player, Pellet pellet) {
-        if (!((Hunter) player).isHunting())
-            super.playerVersusPellet(player, pellet);
+        // This is managed by hunterVersusPellet()
     }
 
     @Override
