d5bd8439fc948a3b8b7d067afa1a0ba1c8b12e02
Maximilien Laptop <neo-sw@hotmail.com>
2016-03-03 14:49:42 +0100

    Ajout de la super-gomme

---
+++ src/main/java/nl/tudelft/jpacman/level/SuperPellet.java
@@ -0,0 +1,18 @@
+package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.sprite.Sprite;
+
+/**
+ * Created by Maximilien on 3/03/16.
+ */
+public class SuperPellet extends Pellet {
+
+    /**
+     * Creates a new super-pellet.
+     * @param points The point value of this super-pellet.
+     * @param sprite The sprite of this super-pellet.
+     */
+    public SuperPellet(int points, Sprite sprite){
+        super(points, sprite);
+    }
+}
