e0bdcc30f899ca7cbab0ff39b805fa44c1a67d85
Maximilien Laptop <neo-sw@hotmail.com>
2016-03-03 20:04:42 +0100

    Ajout javadonc pour le map parser

--- src/main/java/nl/tudelft/jpacman/level/MapParserSuperPellet.java
+++ src/main/java/nl/tudelft/jpacman/level/MapParserSuperPellet.java
@@ -1,10 +1,12 @@
 package nl.tudelft.jpacman.level;
 
 import nl.tudelft.jpacman.PacmanConfigurationException;
+import nl.tudelft.jpacman.board.Board;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.board.Square;
 import nl.tudelft.jpacman.npc.NPC;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,6 +25,29 @@ public class MapParserSuperPellet extends MapParser {
      */
     public MapParserSuperPellet(LevelFactory levelFactory, BoardFactory boardFactory) {
         super(levelFactory,boardFactory);
+    }
+
+    /**
+     * Parses the text representation of the board into an actual level.
+     *
+     * <ul>
+     * <li>Supported characters:
+     * <li>' ' (space) an empty square.
+     * <li>'#' (bracket) a wall.
+     * <li>'.' (period) a square with a pellet.
+     * <li>'P' (capital P) a starting square for players.
+     * <li>'G' (capital G) a square with a ghost.
+     * <li>'*' (star) a square with a superPellet.
+     * </ul>
+     *
+     * @param map
+     *            The text representation of the board, with map[x][y]
+     *            representing the square at position x,y.
+     * @return The level as represented by this text.
+     */
+    @Override
+    public Level parseMap(char[][] map) {
+        return parseMap(map);
     }
 
     @Override
