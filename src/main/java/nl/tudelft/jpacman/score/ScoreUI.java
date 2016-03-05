9720230d38ca0279542918810296af0232403284
Jannou <jannoub@gmail.com>
2016-03-05 18:30:49 +0100

    - implemented code to show hall of fame to user and handle the high scores

--- src/main/java/nl/tudelft/jpacman/score/ScoreUI.java
+++ src/main/java/nl/tudelft/jpacman/score/ScoreUI.java
@@ -9,71 +9,69 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
-
+import javax.swing.BoxLayout;
+import javax.swing.JOptionPane;
 /**
  * JFrame Hall of Frame
  * Created by Jannou on 2/03/16.
  */
+
+
 public class ScoreUI extends JFrame{
-    private static JFrame frmScore;
+    //private static JFrame frmScore;
+    private JPanel mainContentPane ;
+    private HighScorePanel highSP;
+    private HandleScore hScore;
 
-    protected ScoreUI(){
-        initialize();
+    protected ScoreUI(HandleScore hScore){
+        initialize(hScore);
     }
-    private void initialize() {
-        frmScore = new JFrame();
-        frmScore.setTitle("Hall of Fame");
-        //frmScore.setVisible(true);
+    private void initialize(HandleScore hscore) {
+        hScore = hscore;
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Hall of Fame");
+        setSize(370,440);
+        mainContentPane = new JPanel();
+        highSP = new HighScorePanel(hScore);
+        mainContentPane.setLayout(new BoxLayout(mainContentPane, BoxLayout.Y_AXIS));
+        mainContentPane.add(highSP);//add new HighScorePanel
+        mainContentPane.add( new ButtonPan(this));
+        setContentPane(mainContentPane);
+        setVisible(true);
     }
 
+    private class ButtonPan extends JPanel{
+        private JFrame frameScore;
+        public ButtonPan(JFrame frame){
+            this.frameScore = frame;
 
-//
-//    /**
-//     *
-//     */
-//    private static final long serialVersionUID = 879938052274070218L;
-//    private JPanel contentPane = new JPanel();
-//    //private JPanel contentPane2 = new MyScorePanel();
-//    private JPanel contentPane3 = new JPanel();
-//
-//    /**
-//     * Create the frame.
-//     */
-//    protected ScoreUI() {
-//        initialize();
-//    }
-//
-//    /**
-//     * Frame
-//     */
-//    private void initialize(){
-//        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-//        setTitle("Hall of Fame");
-//        setBounds(100, 100, 450, 400);
-//        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-//        contentPane.setLayout(new BorderLayout(0, 0));
-//        contentPane3.setLayout(new BorderLayout(0, 0));
-//        setContentPane(contentPane);
-//        getContentPane().add(contentPane2, BorderLayout.NORTH);
-//        JButton okButton = new JButton("OK");
-//        okButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                System.out.println("yeap");
-//            }
-//        });
-//        JButton resetButton = new JButton("Reset");
-//        resetButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                HandleScore.rest();
-//            }
-//        });
-//        contentPane3.add(okButton, BorderLayout.WEST);
-//        contentPane3.add(resetButton, BorderLayout.EAST);
-//        getContentPane().add(contentPane2, BorderLayout.NORTH);
-//        getContentPane().add(contentPane3, BorderLayout.SOUTH);
-//        setVisible(true);
-//    }
+            JButton okButton = new JButton("OK");
+            okButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
 
+                    frameScore.dispose();
+                }
+            });
+            JButton resetButton = new JButton("Reset");
+            resetButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    int n = 0;
+                    Object[] options = { "Yes", "Cancel" };
+                    n = JOptionPane.showOptionDialog(frame,
+                            "Would you reset Hall of Fame ? all scores will be deleted  ",
+                            "WARNING", JOptionPane.YES_NO_CANCEL_OPTION,
+                            JOptionPane.QUESTION_MESSAGE, null, options,
+                            options[1]);
+                    if(n==0) {
+                        HandleScore.reset();
+                        highSP.reset();
+                    }
+                }
+            });
+        add(okButton);
+        add(resetButton);
+        }
+    }
 }
