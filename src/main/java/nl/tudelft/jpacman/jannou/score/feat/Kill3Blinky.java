5cb643a13ea70f9b5831417be7e89f43f00e4084
Jannou <jannoub@gmail.com>
2016-03-11 17:12:29 +0100

    - a created Kill3Blinky feat

---
+++ src/main/java/nl/tudelft/jpacman/jannou/score/feat/Kill3Blinky.java
@@ -0,0 +1,25 @@
+package nl.tudelft.jpacman.jannou.score.feat;
+
+/**
+ * Created by Jannou on 10/03/16.
+ */
+public class Kill3Blinky extends  Feat {
+
+    public Kill3Blinky(){
+        setName("Kill3Blinky");
+        setValue(100);
+        setDesc("beat Dev1's score");
+        setRealised(true);
+    }
+    public String toMaps(){
+        return  "<feat>\n\t<name>"+getName() + "</name>\n\t<description>" + getDesc() +  "</description>\n\t<value>" + getValue() +
+                "</value>\n\t<realised>" +isRealised() +"<realised>\n</feat>" ;
+    }
+    public boolean condition(int i,int j){
+        return false;
+    }
+
+    @Override
+    public void updatestate(int i) {
+    }
+}
