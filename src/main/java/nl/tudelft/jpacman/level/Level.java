d60a914178d7db750441f901d98a2c63593287d3
Maximilien Laptop <neo-sw@hotmail.com>
2016-03-21 14:13:04 +0100

    Correction problème start and stop npcs

--- src/main/java/nl/tudelft/jpacman/level/Level.java
+++ src/main/java/nl/tudelft/jpacman/level/Level.java
@@ -15,6 +15,7 @@ import nl.tudelft.jpacman.board.Square;
 import nl.tudelft.jpacman.board.Unit;
 import nl.tudelft.jpacman.npc.NPC;
 import nl.tudelft.jpacman.npc.ghost.EatableGhost;
+import nl.tudelft.jpacman.npc.ghost.Ghost;
 
 /**
  * A level of Pac-Man. A level consists of the board with the players and the
@@ -203,6 +204,7 @@ public class Level {
 			if (isInProgress()) {
 				return;
 			}
+			startNPCs();
 			inProgress = true;
 			updateObservers();
 		}
@@ -217,6 +219,7 @@ public class Level {
 			if (!isInProgress()) {
 				return;
 			}
+			stopNPCs();
 			inProgress = false;
 		}
 	}
@@ -226,7 +229,7 @@ public class Level {
 	 */
 	protected void startNPCs() {
 		for (final NPC npc : npcs.keySet()) {
-			setSpeedNPCs(npc, ((EatableGhost) npc).getSpeed());
+			setSpeedNPCs(npc, 1); //default value of speed for ghost is 1
 		}
 	}
 
@@ -269,9 +272,19 @@ public class Level {
 	}
 
 	/**
+	 * Set whether this level is in progress, i.e. whether moves can be made
+	 * on the board.
+	 *
+	 * @param inProgress iff this level is in progress.
+	 */
+	protected void setInProgress(boolean inProgress) {
+		this.inProgress = inProgress;
+	}
+
+	/**
 	 * Updates the observers about the state of this level.
 	 */
-	private void updateObservers() {
+	protected void updateObservers() {
 		if (!isAnyPlayerAlive()) {
 			for (LevelObserver o : observers) {
 				o.levelLost();
